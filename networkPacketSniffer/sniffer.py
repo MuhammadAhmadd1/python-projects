a087e6b8400b1c6e31576c9174cc13540105c61a
MuhammadAhmadd1 <ahmad.james.2315@gmail.com>
2026-09-07 08:55:34 -0400

    Python Network Packet sniffer -  data unpack function

---
+++ networkPacketSniffer/sniffer.py
@@ -0,0 +1,13 @@
+import socket
+import struct 
+import textwrap
+
+# unpack ethernet frame 
+"""
+we see some data flowing across the network and then we pass it into
+this function, when the data is passed it grabs the first 14 bytes(dest, source, type) and 
+unpacks it and then returns all the data after 
+"""
+def ethernet_frame(data):
+    dest_mac, source_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return  get_macdest_mac_addr(dest_mac),get_mac_addr(source_mac), socket.htons(proto), data[14:]
